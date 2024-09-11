0895d2c11fe35a4f28e99809766d44d2194170b6
21CSE091 <111353011+21CSE091@users.noreply.github.com>
2024-09-11 12:18:52 +0530

    Add files via upload

---
+++ EX 10/expt10snowman.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expt10;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+/**
+ *
+ * @author sweth
+ */
+public class expt10snowman extends JPanel {
+    int midX = 400;
+    int midY = 300;
+   public void paintComponent(Graphics g) {
+       super.paintComponent(g);
+       setBackground(new Color(06,03,255));
+       g.setColor (Color.white);
+       g.fillOval (midX-20, midY-60, 40, 40); // head
+       g.fillOval (midX-35, midY-25, 70, 60); // upper body
+       g.fillOval (midX-50, midY+20, 100, 80); // lower body
+       g.setColor (Color.black);
+       g.fillOval (midX-10, midY-50, 5, 5); // left eye
+       g.fillOval (midX+5, midY-50, 5, 5); // right eye
+       g.drawArc (midX-10, midY-40, 20, 10, 190, 160); // smile
+       g.drawLine (midX-25, midY, midX-55, midY); // left arm
+       g.drawLine (midX+25, midY, midX+55, midY); // right arm
+       g.drawLine (midX-20, midY-55, midX+20, midY-55); // brim of hat
+       g.fillRect (midX-15, midY-80, 30, 25); // hat
+}
+public static void main(String args[]) {
+JFrame frame = new JFrame("Snowman");
+frame.add(new expt10snowman());
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setSize(500,500);
+frame.setVisible(true);
+}
+}
+
